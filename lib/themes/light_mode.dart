eb2045708265eba8cd4276da88bcdd3ed8a754af
Mohammed Raquib Adnan KL <klraquib@gmail.com>
2024-08-30 22:56:17 +0530

    updated

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightmode = ThemeData(
-  colorScheme: ColorScheme.light(
-    surface: Colors.grey.shade300,
-    primary: const Color.fromARGB(255, 131, 130, 130),
-    secondary: Color.fromARGB(255, 153, 153, 153),
-    tertiary: const Color.fromARGB(255, 49, 49, 49),
-    inversePrimary: Colors.grey.shade700,
+  colorScheme: const ColorScheme.light(
+    inversePrimary: Color(0xFFFFF6E0), // fonts colors
+    surface: Color(0xFFFFB800), // upper surface
+    tertiary:
+        Color(0xFFFFCD4D), // Secondary color, a lighter shade from primary
+    primary: Color(0xFFFF9100), // Darker shade of primary for tertiary
+    secondary: Color(
+        0xFFFF9100), // Using a similar dark shade from primary for inverse
   ),
 );
