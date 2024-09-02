1c4932d84955deda122a9c30c9dd7fc9ebd74b95
Mohammed Raquib Adnan KL <klraquib@gmail.com>
2024-09-02 19:52:45 +0530

    pending

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 ThemeData lightmode = ThemeData(
   colorScheme: const ColorScheme.light(
     inversePrimary: Color(
-        0xFF5D4037), // Warm Brown for text colors (font), resembling baked goods or spices
+        0xFF333333), // Dark Gray for text colors (font), resembling Swiggy's strong and bold branding
     surface: Color(
-        0xFFFFF9C4), // Light Creamy Yellow for the surface, soft and inviting
+        0xFFF5F5F5), // Light Gray for the surface, soft and neutral background
     tertiary: Color(
-        0xFFFFD54F), // Sunny Yellow for buttons, highlights, creating a bright and cheerful atmosphere
+        0xFFFF5722), // Swiggy Orange for buttons, highlights, representing the brand's energetic and lively nature
     primary: Color(
-        0xFFFFC107), // Golden Yellow for primary elements, representing energy, and richness
+        0xFFFA6400), // Bright Orange for primary elements, matching Swiggy's vibrant and eye-catching look
     secondary: Color(
-        0xFFFFA000), // Deeper Amber for secondary elements, adding depth and contrast
+        0xFFE65100), // Deep Orange for secondary elements, adding richness and warmth
   ),
 );
